37cc46b83d62aedd9a9533d62ac8f4a4641bfdf1
chaimongkol <chaimongkol.aj@gmail.com>
2026-06-28 22:12:03 +0700

    remove builder

--- src/main/java/com/example/userapi/model/User.java
+++ src/main/java/com/example/userapi/model/User.java
@@ -4,14 +4,12 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
